549867e35077268b517e591052652c088d56fef4
Xiaohao XU <65257938+JerryX1110@users.noreply.github.com>
2022-04-29 17:54:30 +0800

    Update build.py

--- networks/encoders/swin/build.py
+++ networks/encoders/swin/build.py
@@ -20,6 +20,17 @@ def build_swin_model(model_type, freeze_at=0):
                                 patch_norm=True,
                                 frozen_stages=freeze_at,
                                 use_checkpoint=False)
+    elif model_type == 'swin_large':
+        model = SwinTransformer(embed_dim=192,
+                                depths=[2, 2, 18, 2],
+                                num_heads=[4, 8, 16, 32],
+                                window_size=7,
+                                drop_path_rate=0.3,
+                                out_indices=(0, 1, 2),
+                                ape=False,
+                                patch_norm=True,
+                                frozen_stages=freeze_at,
+                                use_checkpoint=False)
 
     else:
         raise NotImplementedError(f"Unkown model: {model_type}")
